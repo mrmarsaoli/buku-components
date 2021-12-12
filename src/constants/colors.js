708fbeb89a97a635537326c3040846e51df2c753
Muhaimin Rezki <mrmarsaoli@gmail.com>
2021-12-12 12:26:58 +0900

    Initial commit

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+module.exports = {
+  PRIMARY: '#03ad87',
+  SECONDARY: '#f7b500',
+  WARNING: '#e50707',
+  GRAY: '#f4f4f4',
+  BLUE_80: '#046bb9',
+  BLUE_60: '#0091ff',
+  BLUE_10: '#e5f4ff'
+}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const {
+  PRIMARY,
+  SECONDARY,
+  WARNING,
+  BLUE_80,
+  BLUE_60,
+  BLUE_10,
+  GRAY
+} = require('./src/constants/colors.js')
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,tsx}'],
+  purge: ['./src/**/*.{js,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: PRIMARY,
+        secondary: SECONDARY,
+        gray: {
+          DEFAULT: GRAY
+        },
+        warning: WARNING,
+        blue: {
+          80: BLUE_80,
+          60: BLUE_60,
+          10: BLUE_10
+        }
+      }
+    }
+  },
+  plugins: []
+}
